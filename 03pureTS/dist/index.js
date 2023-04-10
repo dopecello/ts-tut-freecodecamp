8c73d124980948347170ccc49a256b283178c462
dopecello <williamlocke.cello@gmail.com>
2023-04-10 14:55:22 -0400

    learning private and public syntax for objects. syntax sugar ex

--- 03pureTS/dist/index.js
+++ 03pureTS/dist/index.js
@@ -1,10 +1,18 @@
 "use strict";
 class User {
     constructor(email, name) {
-        this.city = ""; //i wont get an error if I dont pass this into the constructor
+        this.city = "";
         this.email = email;
         this.name = name;
     }
 }
 const william = new User('wcl@gmail.com', "william");
 william.city = "Jax"; // <=== I shouldnt be able to do this. Thus, readonly. 
+//MORE REAL_WORLD CASE TO WRITE THIS
+class Other {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "Jax";
+    }
+} //produces same code in the dist folder essentially. syntax sugar. cleaner to me than using 'this'
--- 03pureTS/src/index.ts
+++ 03pureTS/src/index.ts
@@ -1,13 +1,25 @@
 class User {
-    email: string;
-    name: string;
-    readonly city: string = "" //i wont get an error if I dont pass this into the constructor
+    public email: string; //using public will make it so that I can access this outside the class.
+    private name: string; //using private ensures that this param can only be accessed within the class.
+    readonly city: string = ""
     constructor(email: string, name: string) {
         this.email = email;
         this.name = name
     }
 }
 
-const william = new User ('wcl@gmail.com',"william")
+const william = new User('wcl@gmail.com', "william")
 william.city = "Jax" // <=== I shouldnt be able to do this. Thus, readonly. 
- 
+
+
+//MORE REAL_WORLD CASE TO WRITE THIS
+
+class Other {
+    readonly city: string = "Jax"
+    constructor(
+        public email: string,
+        public name: string
+    ) {
+
+    }
+} //produces same code in the dist folder essentially. syntax sugar. cleaner to me than using 'this'
