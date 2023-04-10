1c5aacb3f59b607cd6867d01b8580bb5cb512e42
dopecello <williamlocke.cello@gmail.com>
2023-04-10 15:06:15 -0400

    getters and setters

--- 03pureTS/dist/index.js
+++ 03pureTS/dist/index.js
@@ -9,10 +9,26 @@ class User {
 const william = new User('wcl@gmail.com', "william");
 william.city = "Jax"; // <=== I shouldnt be able to do this. Thus, readonly. 
 //MORE REAL_WORLD CASE TO WRITE THIS
-class Other {
+class OtherUser {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = "Jax";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`; //this is what is known as a getter.
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 } //produces same code in the dist folder essentially. syntax sugar. cleaner to me than using 'this'
--- 03pureTS/src/index.ts
+++ 03pureTS/src/index.ts
@@ -14,12 +14,33 @@ william.city = "Jax" // <=== I shouldnt be able to do this. Thus, readonly.
 
 //MORE REAL_WORLD CASE TO WRITE THIS
 
-class Other {
+class OtherUser {
+
+    private _courseCount = 1
+
     readonly city: string = "Jax"
     constructor(
         public email: string,
         public name: string
     ) {
+    }
 
+    private deleteToken(){
+        console.log("Token deleted");
+    }
+    
+    get getAppleEmail(): string{
+        return `apple${this.email}` //this is what is known as a getter.
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum)/*: void */ { // will throw error if i set return type to void beacuse A 'set' accessor cannot have a return type annotation.  
+        if (courseNum <= 1) {
+            throw new Error("course count should be more than 1")
+        }
+        this._courseCount = courseNum
     }
 } //produces same code in the dist folder essentially. syntax sugar. cleaner to me than using 'this'
