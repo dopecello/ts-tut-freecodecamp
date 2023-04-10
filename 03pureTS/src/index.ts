aea7f6647d26bdc13ffd82a2c123450164f7fee3
dopecello <williamlocke.cello@gmail.com>
2023-04-10 14:35:33 -0400

    learned watch mode in ts

---
+++ 03pureTS/src/index.ts
@@ -0,0 +1,6 @@
+console.log("ts is here still")
+
+//tsc -w turns on watch mode nad with the outDir configuration in tsconfig.json, it automatically compiles
+// the changes into js within the dist folder on save
+
+//lite-server is a good package t get a live server without using live server or nodemon.
