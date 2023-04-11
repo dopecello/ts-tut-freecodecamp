72c4676360b9d67e4a69d68f33b168b68fa74df4
dopecello <williamlocke.cello@gmail.com>
2023-04-10 22:59:48 -0400

    adding compiled files from generics intro

--- 03pureTS/dist/abstractClass.js
+++ 03pureTS/dist/abstractClass.js
@@ -12,7 +12,7 @@ class TakePhoto {
 //these are kind of like blueprints. it's the responsibility of other classes to extend TakePhoto
 class Instagram extends TakePhoto {
     constructor(cameraMode, filter, burst) {
-        super(cameraMode, filter);
+        super(cameraMode, filter); //needs to be included 
         this.cameraMode = cameraMode;
         this.filter = filter;
         this.burst = burst;
---
+++ 03pureTS/dist/myGenerics.js
@@ -0,0 +1,30 @@
+"use strict";
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+} // I can narrow this, but doesnt cut through the problem were facing, especially with string.
+function identityTwo(val) {
+    return val; //loses information in this way, we don't know what we are returning. plus, any is cringe!!
+}
+function identityThree(val) {
+    return val;
+} // this is still kind of like any. ready to accept numbers, string, w.e, but this value is logged.
+identityThree(3); //hover this
+identityThree('3'); // and this
+function identityFour(val) {
+    return val;
+}
+identityFour(Object.assign({}, )); //this is the sytnax if i created my own.
+function getSearchProducts(products) {
+    // do some db ops
+    const myIndex = 3;
+    return products[myIndex]; //this is correct because the return is locked by the input. has to be an array
+}
+const getMoreSearchProducts = (products) => {
+    // do db ops
+    const myIndex = 4;
+    return products[myIndex];
+}; // generic function but in an arrow function.
+//you will commonly get a <T,> syntax because sometimes this tag can be confused for HTML/JSX. In professional codebases
+//this is a common notation. It will not product an error.
